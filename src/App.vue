8c0fff5bef818af532895feb90fc87a9b260c623
JavierPoduje <javier.poduje@gmail.com>
2023-05-12 18:45:45 -0400

    feat: Initial setup

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Programming challenge</h1>
+  <div class="hola">hola</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.hola {
+  border: 1px solid red;
+
+}
+</style>
