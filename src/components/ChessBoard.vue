dff2e122cb25017bfe78c6017ffd8dc086e61ffb
JavierPoduje <javier.poduje@gmail.com>
2023-05-12 21:03:15 -0400

    feat: console.log cells

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -2,7 +2,11 @@
   <div class="chessboard">
     <div v-for="(row, rowIdx) in rows" :key="row" class="chessboard__row">
       <div v-for="(col, colIdx) in cols" :key="col" class="chessboard__col">
-        <div class="chessboard__cell" :class="cellColor(rowIdx, colIdx)"></div>
+        <div
+          class="chessboard__cell"
+          :class="cellColor(rowIdx, colIdx)"
+          @click="onCellClick(row, col)"
+        ></div>
       </div>
     </div>
   </div>
@@ -13,20 +17,21 @@
     name: 'ChessBoard',
     data() {
       return {
-        rows: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
-        cols: [1, 2, 3, 4, 5, 6, 7, 8],
+        cols: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+        rows: [8, 7, 6, 5, 4, 3, 2, 1],
       };
     },
     methods: {
-      isLightCell(rowIdx: number, colIdx: number): boolean {
-        return (rowIdx + colIdx) % 2 === 0;
-      },
       cellColor(rowIdx: number, colIdx: number): string {
-        if (this.isLightCell(rowIdx, colIdx)) {
+        const isLightCell = (rowIdx + colIdx) % 2 === 0;
+        if (isLightCell) {
           return 'chessboard__cell--light';
         } else {
           return 'chessboard__cell--dark';
         }
+      },
+      onCellClick(row: string, col: number) {
+        console.log(col, row);
       },
     },
   };
@@ -63,6 +68,10 @@
       }
       &--dark {
         background-color: map-get($colors, dark-cell);
+      }
+
+      &:active {
+        box-shadow: inset 0px 0px 0px 5px #fff;
       }
     }
   }
